30247d6927869b9b48855db03d10af8ac0caef08
Dennis Reimann <mail@dennisreimann.de>
2023-08-24 15:09:48 +0200

    Improve sample test

--- BTCPayApp.Tests/UnitTest1.cs
+++ BTCPayApp.Tests/UnitTest1.cs
@@ -13,25 +13,18 @@ public class UnitTest1
     }
 
     [Fact]
-    public async Task Test1()
+    public async Task TestHomePage()
     {
         await using var factory = new BTCPayAppTestServer(_testOutputHelper);
         var page = await (await factory.InitializeAsync()).NewPageAsync();
         await page.GotoAsync(factory.ServerAddress);
-        factory.Eventually(() =>
-        {
-            Assert.EndsWith(Routes.Home, page.Url);
-        });
+        Assert.EndsWith(Routes.Home, page.Url);
 
-        Thread.Sleep(3000);
-        var carousel = await page.QuerySelectorAsync("#OnboardingCarousel");
-        await (await carousel.QuerySelectorAsync("[aria-label='3']")).ClickAsync();
-        Thread.Sleep(3000);
-        await (await page.QuerySelectorAsync("#PairButton")).ClickAsync();
-        factory.Eventually(() =>
-        {
-            Assert.EndsWith(Routes.Pair, page.Url);
-        });
+        var carousel = page.Locator("#OnboardingCarousel");
+        await carousel.Locator("[aria-label='3']").ClickAsync();
+        Assert.True(await carousel.GetByTestId("StandaloneButton").IsDisabledAsync());
 
+        await carousel.GetByTestId("PairButton").ClickAsync();
+        Assert.EndsWith(Routes.Pair, page.Url);
     }
 }
