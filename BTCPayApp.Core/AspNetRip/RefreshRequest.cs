b7df8434c54b167453c2e1823ed0b56b2eacab86
Kukks <evilkukka@gmail.com>
2024-04-15 11:32:37 +0200

    try fix maui

---
+++ BTCPayApp.Core/AspNetRip/RefreshRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Microsoft.AspNetCore.Identity.Data;
+
+/// <summary>
+/// The request type for the "/refresh" endpoint added by <see cref="IdentityApiEndpointRouteBuilderExtensions.MapIdentityApi"/>.
+/// </summary>
+public sealed class RefreshRequest
+{
+    /// <summary>
+    /// The <see cref="AccessTokenResponse.RefreshToken"/> from the last "/login" or "/refresh" response used to get a new <see cref="AccessTokenResponse"/>
+    /// with an extended expiration.
+    /// </summary>
+    public required string RefreshToken { get; init; }
+}
