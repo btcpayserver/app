3663e890e63931326e9395519c17cddf0ce9f6cd
Kukks <evilkukka@gmail.com>
2024-04-23 14:36:38 +0200

    wip

---
+++ BTCPayApp.Core/LDK/LDKSignerProvider.cs
@@ -0,0 +1,60 @@
+﻿using BTCPayApp.Core.Data;
+using BTCPayApp.Core.LDK;
+using NBitcoin;
+using NLDK;
+using org.ldk.structs;
+using org.ldk.util;
+using UInt128 = org.ldk.util.UInt128;
+
+namespace nldksample.LDK;
+
+public class LDKSignerProvider : SignerProviderInterface
+{
+    private readonly CurrentWalletService _currentWalletService;
+    private readonly SignerProvider _innerSigner;
+
+    public LDKSignerProvider(CurrentWalletService currentWalletService,  KeysManager innerSigner)
+    {
+        _currentWalletService = currentWalletService;
+        _innerSigner = innerSigner.as_SignerProvider();
+    }
+
+    public byte[] generate_channel_keys_id(bool inbound, long channel_value_satoshis, UInt128 user_channel_id)
+    {
+        return _innerSigner.generate_channel_keys_id(inbound, channel_value_satoshis, user_channel_id);
+    }
+
+    public WriteableEcdsaChannelSigner derive_channel_signer(long channel_value_satoshis, byte[] channel_keys_id)
+    {
+        return _innerSigner.derive_channel_signer(channel_value_satoshis, channel_keys_id);
+    }
+
+    public Result_WriteableEcdsaChannelSignerDecodeErrorZ read_chan_signer(byte[] reader)
+    {
+        return _innerSigner.read_chan_signer(reader);
+    }
+
+    public Result_CVec_u8ZNoneZ get_destination_script(byte[] channel_keys_id)
+    {
+        var script = _currentWalletService.DeriveScript().GetAwaiter().GetResult();
+        return Result_CVec_u8ZNoneZ.ok(script.ToScript().ToBytes());
+    }
+
+    public Result_ShutdownScriptNoneZ get_shutdown_scriptpubkey()
+    {
+        var script = _currentWalletService.DeriveScript().GetAwaiter().GetResult();
+        var s = script.ToScript();
+
+
+        if (!s.IsScriptType(ScriptType.Witness))
+        { throw new NotSupportedException("Generated a non witness script."); }
+
+
+        var witnessParams = PayToWitTemplate.Instance.ExtractScriptPubKeyParameters2(s);
+        var result = ShutdownScript.new_witness_program(new WitnessProgram(witnessParams.Program,
+            new WitnessVersion((byte) witnessParams.Version)));
+        if(result is Result_ShutdownScriptInvalidShutdownScriptZ.Result_ShutdownScriptInvalidShutdownScriptZ_OK ok)
+            return Result_ShutdownScriptNoneZ.ok(ok.res);
+        return Result_ShutdownScriptNoneZ.err();
+    }
+}
