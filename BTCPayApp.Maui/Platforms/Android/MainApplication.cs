96577625166d0748e5f7e956e014490a702a7b4f
Dennis Reimann <mail@dennisreimann.de>
2024-04-30 16:54:08 +0200

    Android: Allow clearnet connections with DEBUG flag

--- BTCPayApp.Maui/Platforms/Android/MainApplication.cs
+++ BTCPayApp.Maui/Platforms/Android/MainApplication.cs
@@ -1,15 +1,14 @@
 ﻿using Android.App;
 using Android.Runtime;
 
-namespace BTCPayApp.Maui.Platforms.Android;
+namespace BTCPayApp.Maui;
 
+#if DEBUG
+[Application(UsesCleartextTraffic = true)]
+#else
 [Application]
-public class MainApplication : MauiApplication
+#endif
+public class MainApplication(IntPtr handle, JniHandleOwnership ownership) : MauiApplication(handle, ownership)
 {
-    public MainApplication(IntPtr handle, JniHandleOwnership ownership)
-        : base(handle, ownership)
-    {
-    }
-
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
