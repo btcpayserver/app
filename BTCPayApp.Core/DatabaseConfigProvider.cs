7502d1f6748bfcc435ca5a100fc2d2407714e73e
Kukks <evilkukka@gmail.com>
2024-05-04 15:49:13 +0200

    fix channel serializer

---
+++ BTCPayApp.Core/DatabaseConfigProvider.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json;
+using BTCPayApp.Core.Contracts;
+using BTCPayApp.Core.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BTCPayApp.Core;
+
+public class DatabaseConfigProvider: IConfigProvider
+{
+    private readonly IDbContextFactory<AppDbContext> _dbContextFactory;
+
+    public DatabaseConfigProvider(IDbContextFactory<AppDbContext> dbContextFactory)
+    {
+        _dbContextFactory = dbContextFactory;
+    }
+
+    public async Task<T?> Get<T>(string key)
+    {
+        await using var dbContext = await _dbContextFactory.CreateDbContextAsync();
+        var config = await dbContext.Settings.FindAsync(key);
+        if (typeof(T) == typeof(byte[]))
+            return (T?) (config?.Value as object);
+        return config is null ? default : JsonSerializer.Deserialize<T>(config.Value);
+    }
+
+    public async Task Set<T>(string key, T? value)
+    {
+        await using var dbContext = await _dbContextFactory.CreateDbContextAsync();
+        if (value is null)
+        {
+            try
+            {
+
+                dbContext.Settings.Remove(new Setting() {Key = key});
+                await dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+            }
+            return;
+        }
+
+        var newValue =  typeof(T) == typeof(byte[])? value as byte[]:JsonSerializer.SerializeToUtf8Bytes(value);
+        await dbContext.Upsert(new Setting() {Key = key, Value = newValue}).RunAsync();
+
+    }
+}
--- BTCPayApp.Core/StartupExtensions.cs
+++ BTCPayApp.Core/StartupExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System.Text.Json;
-using BTCPayApp.CommonServer;
+﻿using BTCPayApp.CommonServer;
 using BTCPayApp.Core.Attempt2;
 using BTCPayApp.Core.Contracts;
 using BTCPayApp.Core.Data;
@@ -42,47 +41,6 @@ public static class StartupExtensions
         serviceCollection.AddLDK();
 
         return serviceCollection;
-    }
-}
-
-public class DatabaseConfigProvider: IConfigProvider
-{
-    private readonly IDbContextFactory<AppDbContext> _dbContextFactory;
-
-    public DatabaseConfigProvider(IDbContextFactory<AppDbContext> dbContextFactory)
-    {
-        _dbContextFactory = dbContextFactory;
-    }
-
-    public async Task<T?> Get<T>(string key)
-    {
-        await using var dbContext = await _dbContextFactory.CreateDbContextAsync();
-        var config = await dbContext.Settings.FindAsync(key);
-        if (typeof(T) == typeof(byte[]))
-            return (T?) (config?.Value as object);
-        return config is null ? default : JsonSerializer.Deserialize<T>(config.Value);
-    }
-
-    public async Task Set<T>(string key, T? value)
-    {
-        await using var dbContext = await _dbContextFactory.CreateDbContextAsync();
-        if (value is null)
-        {
-            try
-            {
-
-                dbContext.Settings.Remove(new Setting() {Key = key});
-                await dbContext.SaveChangesAsync();
-            }
-            catch (DbUpdateException e)
-            {
-            }
-            return;
-        }
-
-        var newValue = JsonSerializer.SerializeToUtf8Bytes(value);
-        await dbContext.Upsert(new Setting() {Key = key, Value = newValue}).RunAsync();
-
     }
 }
 
