5c5369e373dce450edeed4b8588be284ce51b9b1
Kukks <evilkukka@gmail.com>
2024-05-28 11:44:46 +0200

    jit wip

--- BTCPayApp.Core/Data/LightningConfig.cs
+++ BTCPayApp.Core/Data/LightningConfig.cs
@@ -10,6 +10,8 @@ public class LightningConfig
     public string LightningDerivationPath { get; set; } = "m/666'";// your lightning node derivation path
     public string Color { get; set; } = "#51B13E";
 
+    public string? JITLSP { get; set; } // Just In Time Lightning Service Provider
+
     [JsonIgnore]
     public byte[] RGB
     {
---
+++ BTCPayApp.Core/LSP/JIT/BTCPayJIT.cs
@@ -0,0 +1,25 @@
+﻿using BTCPayApp.Core.Attempt2;
+using BTCPayServer.Lightning;
+
+namespace BTCPayApp.Core.LSP.JIT;
+
+public class BTCPayJIT: IJITService
+{
+    public BTCPayJIT(BTCPayConnectionManager btcPayConnectionManager)
+    {
+        
+    }
+
+    public string ProviderName => "BTCPayServer";
+
+    public async Task<BOLT11PaymentRequest> WrapInvoice(BOLT11PaymentRequest invoice)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public interface IJITService
+{
+    public string ProviderName { get; }
+    public Task<BOLT11PaymentRequest> WrapInvoice(BOLT11PaymentRequest invoice);
+}
