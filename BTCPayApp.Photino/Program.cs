4257d85bd99d0579c94b965950c160c206945640
Kukks <evilkukka@gmail.com>
2024-11-27 10:39:31 +0100

    try fix photino and ios build

--- BTCPayApp.Photino/Program.cs
+++ BTCPayApp.Photino/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Photino.Blazor;
-using PhotinoNET;
+using Photino.NET;
 
 namespace BTCPayApp.Photino;
 
