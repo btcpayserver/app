19d7bb8736950f8aff9200583b94dda3c30f3c42
Dennis Reimann <mail@dennisreimann.de>
2025-02-04 15:40:30 +0100

    Updates for Android build
    Fixes #143.

--- BTCPayApp.Maui/Platforms/Android/MainApplication.cs
+++ BTCPayApp.Maui/Platforms/Android/MainApplication.cs
@@ -4,16 +4,13 @@ using Android.Runtime;
 namespace BTCPayApp.Maui;
 
 #if DEBUG
-[Application(
-    UsesCleartextTraffic = true,
-    Debuggable = true
-    )]
+[Application(UsesCleartextTraffic = true, Debuggable = true)]
 #else
 [Application]
 #endif
 public class MainApplication(IntPtr handle, JniHandleOwnership ownership) : MauiApplication(handle, ownership)
 {
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-    
-    
+
+
 }
