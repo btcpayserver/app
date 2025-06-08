ac98a426c0590c5f6dacd5bc85fc2fe3f80da3fa
Ghander <cen254@gmail.com>
2025-06-08 15:28:14 -0500

    enable web-nfc for checkout iframe

--- BTCPayApp.Maui/Platforms/Android/MainActivity.cs
+++ BTCPayApp.Maui/Platforms/Android/MainActivity.cs
@@ -11,31 +11,5 @@ namespace BTCPayApp.Maui;
                            ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
-    protected override void OnCreate(Android.OS.Bundle? savedInstanceState)
-    {
-        base.OnCreate(savedInstanceState);
-        // Initialize NFC Plugin  
-        CrossNFC.Init(this);
-    }
 
-    protected override void OnResume()
-    {
-        base.OnResume();
-        CrossNFC.OnResume();
-
-        var nfcAdapter = NfcAdapter.GetDefaultAdapter(this);
-        if (nfcAdapter != null)
-        {
-            var intent = new Intent(this, GetType()).AddFlags(ActivityFlags.SingleTop);
-            var pendingIntent = PendingIntent.GetActivity(this, 0, intent, PendingIntentFlags.Immutable);
-            nfcAdapter.EnableForegroundDispatch(this, pendingIntent, null, null);
-        }
-    }
-
-    protected override void OnNewIntent(Intent? intent)
-    {
-        base.OnNewIntent(intent);
-        // Handle NFC tag discovery
-        CrossNFC.OnNewIntent(intent);
-    }
 }
